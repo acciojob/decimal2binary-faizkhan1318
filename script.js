44a8f50ab30458f5b5ebbc7ca94e896bbd7f96bf
Faisel Khan <125124484+faizkhan1318@users.noreply.github.com>
2023-09-14 16:19:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function decimalToBinary(num) {
   //Write you code here
+	if(num === 0){
+        return 0;
+    }
+
+    let s ='';
+
+    while(num > 0){
+        s = (num % 2) +s;
+        num = Math.floor(num/2);
+    }
+    return s;
   
 }
 
